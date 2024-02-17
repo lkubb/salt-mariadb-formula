5ba274b52903736faf2c21afa8142f50848aee30
jeanluc <lkubb@protonmail.com>
2024-02-17 19:16:25 +0100

    fix: Update template

--- test/integration/default/controls/config.rb
+++ test/integration/default/controls/config.rb
@@ -24,10 +24,10 @@ control 'mariadb.config.file' do
       # rubocop:disable Lint/RedundantCopDisableDirective
       # rubocop:disable Layout/LineLength
       should include(
-        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", '\
-        '"roles", "osfinger", "os", "os_family"], "source_files": '\
-        '{"mariadb-config-file-file-managed": ["example.tmpl.jinja"], '\
-        '"mariadb--config-file-file-managed": '\
+        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", ' \
+        '"roles", "osfinger", "os", "os_family"], "source_files": ' \
+        '{"mariadb-config-file-file-managed": ["example.tmpl.jinja"], ' \
+        '"mariadb--config-file-file-managed": ' \
         '["-example.tmpl.jinja"]}'
       )
       # rubocop:enable Layout/LineLength
